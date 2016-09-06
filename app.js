b5427fbb59c70407bb9dddd0ef72428f52fcb539
Adam Palaniuk <Adam.Palaniuk@D2L.com>
2016-09-05 22:59:37 -0500

    Add CLI; Change from array to map for simple "exists" check

--- app.js
+++ app.js
@@ -13,6 +13,24 @@ Clarifai.initialize({
   'clientSecret': 'p2i9cgUjXse1ok-H6bcisiAM_0aKv5FfcZFzAaxJ'
 });
 
+const replServer = repl.start({
+    prompt: '> ',
+    eval: eval
+});
+
+function convertToMap(outfile) {
+    var convertedResults = {};
+
+    for(var i = 0; i < clarifaiResults.length; i++) {
+        convertedResults[clarifaiResults[i].file] = clarifaiResults[i].response;
+    }
+
+    fs.writeFileSync(
+        outfile, 
+        JSON.stringify(convertedResults, null, 4)
+    );
+};
+
 var walk = function(dir, done) {
   var files = [];
   fs.readdir(dir, function(err, list) {
@@ -40,26 +58,26 @@ var walk = function(dir, done) {
 function saveResponses() {
     fs.writeFileSync(
         'C:\\clarifai_results.txt', 
-        JSON.stringify(clarifaiResults, null, 4),
-        function(err) {
-            if(err) {
-                return console.log(err);
-            }
-        }
+        JSON.stringify(clarifaiResults, null, 4)
     );
 };
 
-walk("C:\\Users\\apalaniuk\\Desktop\\final-images", function(err, files) {
-  if (err) throw err;
+function main() {
+    walk("C:\\Users\\apalaniuk\\Desktop\\final-images", function(err, files) {
+        if (err) {
+            saveResponses();
+            throw err;
+        }
 
-  for(var i = 0; i < files.length; i++) {
-      if(files[i].indexOf('MobileBanner_767x490@1x.jpg') !== -1) {
-        inputFiles.push(files[i]);
-      }
-  }
+        for(var i = 0; i < files.length; i++) {
+            if(files[i].indexOf('MobileBanner_767x490@1x.jpg') !== -1) {
+                inputFiles.push(files[i]);
+            }
+        }
 
-  getTags();
-});
+        getTags();
+    });
+}
 
 function getTags() {
     fs.readFile(inputFiles[currImgIndex], function(err, data) {
@@ -95,8 +113,6 @@ function handleResponse(response){
     if (++currImgIndex < inputFiles.length) {
         setTimeout(getTags, 1000);
     } else {
-        //console.log('** FINAL RESULTS **');
-        //console.log(JSON.stringify(clarifaiResults, null, 4));
         saveResponses();
         console.log("Complete! I hope.");
     }
@@ -106,3 +122,21 @@ function handleError(err){
   console.log('promise error: ', err);
   saveResponses();
 };
+
+function eval(cmd, context) {
+    var args = cmd.trim().split(' ');
+
+    if (args[0] === 'load') {
+        //console.log(context);
+        fs.readFile(args[1], function(err, data) {
+           clarifaiResults = JSON.parse(data); 
+           console.log("Read " + clarifaiResults.length + ' entries');
+        });
+    } else if(args[0] === 'print') {
+        console.log(JSON.stringify(clarifaiResults, null, 4));
+    } else if(args[0] === 'convert') {
+        convertToMap(args[1].trim());
+    }
+
+    replServer.displayPrompt();
+}
